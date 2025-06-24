dc1ec54470c784a6de75afa6593892648cfd6f7d
Arman Tadevosyan <gittadevosyan889@gmail.com>
2025-06-24 22:39:50 +0400

    Removed unnecessary files.

--- packages/server/src/routes/api.router.ts
+++ packages/server/src/routes/api.router.ts
@@ -6,28 +6,3 @@ const apiRouter = Router();
 apiRouter.post("/save-form", saveFormController);
 
 export { apiRouter };
-
-// import { Router, Request, Response } from "express";
-// import { ref, set } from "firebase/database";
-// import { db } from "../config/firebase";
-
-// const apiRouter = Router();
-
-// apiRouter.post("/save-form", async (req: Request, res: Response) => {
-//   try {
-//     const formData = req.body;
-//     const dataRef = ref(db, `forms/${formData.form_id}`);
-//     await set(dataRef, formData);
-
-//     res
-//       .status(200)
-//       .json({ message: "Form data saved successfully", data: formData });
-//   } catch (error) {
-//     res.status(500).json({
-//       message: "Failed to save form data",
-//       error: (error as Error).message,
-//     });
-//   }
-// });
-
-// export { apiRouter };
